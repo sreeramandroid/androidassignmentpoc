abd29d59c1c30cb97b494d2539db9c7cd379f149
shankaram527 <shankaram.gollakota@gmail.com>
2021-05-11 16:16:15 +0530

    Refactoring Code

--- app/src/main/java/com/poc/androidassignment/utils/NetworkUtils.kt
+++ app/src/main/java/com/poc/androidassignment/utils/NetworkUtils.kt
@@ -7,9 +7,9 @@ import android.os.Build
 
 class NetworkUtils {
 
-    /*
- *function to check the network availability or not
- * **/
+    /**
+     * Function to check for Online Connection.
+     */
     fun isOnline(context: Context): Boolean {
         val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
